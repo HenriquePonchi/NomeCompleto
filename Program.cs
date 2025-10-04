2004111d833f9157bfbc17771b1bd6976d75a73a
Luiz <luizponchi3@gmail.com>
2025-10-03 22:01:26 -0300

    FINALIZADO

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nome;
+string sobrenome;
+
+
+Console.Write("Informe o seu nome: ");
+nome = Console.ReadLine()!;
+
+
+Console.Write("Informe o seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+
+
+string nomeCatalogo = $"Nome de catálogo: {sobrenome.ToUpper()}, {nome}";
+Console.WriteLine(nomeCatalogo);
